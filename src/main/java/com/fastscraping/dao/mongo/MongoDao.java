8d42e66f06878901da6e572c20c5f25b84fefb63
Ashish Tomer <ashish1269@zoho.com>
2018-09-20 21:34:13 +0530

    Replaced the async driver with sync driver for mongo db

--- src/main/java/com/fastscraping/dao/mongo/MongoDao.java
+++ src/main/java/com/fastscraping/dao/mongo/MongoDao.java
@@ -2,28 +2,23 @@ package com.fastscraping.dao.mongo;
 
 
 import com.fastscraping.dao.PersistentDaoInf;
-import com.fastscraping.models.ActionsAndData;
 import com.fastscraping.models.ScrapingInformation;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.ServerAddress;
-import com.mongodb.async.client.MongoClient;
-import com.mongodb.async.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import com.mongodb.connection.ClusterSettings;
 import com.mongodb.connection.ConnectionPoolSettings;
 import com.mongodb.connection.netty.NettyStreamFactoryFactory;
-import org.bson.codecs.configuration.CodecRegistry;
-import org.bson.codecs.pojo.PojoCodecProvider;
 
-import java.net.MalformedURLException;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import static com.fastscraping.util.ScraperThreadPools.persistDBThreadPool;
-import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
-import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 public class MongoDao implements PersistentDaoInf {
 
@@ -46,14 +41,10 @@ public class MongoDao implements PersistentDaoInf {
                 .maxConnectionIdleTime(5, TimeUnit.SECONDS)
                 .build();
 
-        CodecRegistry pojoCodecRegistry = fromRegistries(MongoClients.getDefaultCodecRegistry(),
-                fromProviders(PojoCodecProvider.builder().automatic(true).build()));
-
         MongoClientSettings settings = MongoClientSettings.builder()
                 .applyToClusterSettings(builder -> builder.applySettings(clusterSettings))
                 .applyToConnectionPoolSettings(builder -> builder.applySettings(poolSettings))
                 .streamFactoryFactory(NettyStreamFactoryFactory.builder().build())
-                .codecRegistry(pojoCodecRegistry)
                 .build();
 
         mongoClient = MongoClients.create(settings);
@@ -104,7 +95,10 @@ public class MongoDao implements PersistentDaoInf {
         System.out.println("Submiting job to executor to get scraping information.");
         return persistDBThreadPool.submit(() -> scrapingInformationDB.getScrapingInformation(clientId, jobId)
                 .stream()
-                .filter(info -> info.getClientId() == clientId && info.getJobId() == jobId)
+                .filter(info -> info.isPresent() &&
+                        info.get().getClientId().equals(clientId) &&
+                        info.get().getJobId().equals(jobId))
+                .map(Optional::get) // The present scraping information has already been filtered so can call .get()
                 .collect(Collectors.toList()));
     }
 
--- src/main/java/com/fastscraping/dao/mongo/ScrapingInformationDB.java
+++ src/main/java/com/fastscraping/dao/mongo/ScrapingInformationDB.java
@@ -2,16 +2,17 @@ package com.fastscraping.dao.mongo;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fastscraping.models.ScrapingInformation;
-import com.mongodb.async.client.FindIterable;
-import com.mongodb.async.client.MongoClient;
-import com.mongodb.async.client.MongoDatabase;
-import com.mongodb.async.client.MongoIterable;
+import com.mongodb.Block;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Indexes;
-import com.mongodb.client.model.UpdateOptions;
+import com.mongodb.client.model.ReplaceOptions;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
+import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
@@ -38,10 +39,7 @@ public class ScrapingInformationDB {
         database = mongoClient.getDatabase(dbName); //TODO: Mention db name in properties file?
         database.getCollection(scrapingInformationByClientidJobid)
                 .createIndex(Indexes.compoundIndex(Indexes.descending("clientId"),
-                        Indexes.descending("jobId")), (res, ex) -> {
-                    ex.printStackTrace();
-                    System.out.println("Exception while creating the index in Mongo DB");
-                });
+                        Indexes.descending("jobId")));
     }
 
     public void addScrapingInformation(ScrapingInformation information) {
@@ -54,17 +52,30 @@ public class ScrapingInformationDB {
         database.getCollection(scrapingInformationByClientidJobid)
                 .replaceOne(and(eq("clientId", clientId), eq("jobId", jobId)),
                         doc,
-                        new UpdateOptions().upsert(true),
-                        (result, ex) -> System.out.println("The information has not been saved in the Mongo database. "
-                                + ex.getMessage()));
+                        new ReplaceOptions().upsert(true));
     }
 
-    public MongoIterable<ScrapingInformation> getScrapingInformation(String clientId, String jobId) {
-        List<ScrapingInformation> scrapingInformation = new LinkedList<>();
+    public List<Optional<ScrapingInformation>> getScrapingInformation(String clientId, String jobId) {
 
-        FindIterable<Document> scrapingInfo = database.getCollection(scrapingInformationByClientidJobid)
-                .find(and(eq("clientId", clientId), eq("jobId", jobId)))
-                .maxAwaitTime(2, TimeUnit.SECONDS);
+        FindIterable<Document> documents = database.getCollection(scrapingInformationByClientidJobid)
+                .find(and(eq("clientId", clientId), eq("jobId", jobId)));
+
+        List<Optional<ScrapingInformation>> scrapingInformation = new ArrayList<>();
+
+        for(Document document: documents) {
+            try {
+                scrapingInformation.add(Optional.of(getScrapingInformationFromJson(document.toJson(MongoConfig.settings),
+                        new TypeReference<ScrapingInformation>() {
+                        })));
+                System.out.println("......................................" + scrapingInformation.size());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Populated the linked with documents " + scrapingInformation.size());
+
+        return scrapingInformation;
     }
 
     public void addLinksToScrape(String clientId, String jobId, List<String> links) {
@@ -75,19 +86,14 @@ public class ScrapingInformationDB {
         mapToInsert.put("links", links);
 
         database.getCollection(linksToScrapebyClientIdJobId)
-                .insertOne(new Document(mapToInsert), (result, ex) -> {
-                    System.out.println("There was error while trying to insert links to scrape.");
-                });
+                .insertOne(new Document(mapToInsert));
     }
 
     public List<String> getLinksToScrape(String clientId, String jobId) {
         List<String> links = new LinkedList<>();
         database.getCollection(linksToScrapebyClientIdJobId)
                 .find(filterWithClientIdJobId(clientId, jobId), String.class)
-                .forEach(links::add, ((result, ex) -> {
-                    System.out.println("Error while trying to find the links to scrape from the " +
-                            "Mongo DB. " + ex.getMessage());
-                }));
+                .forEach((Block<String>) links::add);
         return links;
     }
 
@@ -99,10 +105,7 @@ public class ScrapingInformationDB {
 
         List<String> links = new LinkedList<>();
 
-        result.forEach(links::add, (res, ex) -> System.out.println("Exception while getting unscraped " +
-                "links from Mongo. " + ex.getMessage())
-        );
-
+        result.forEach((Block<String>) links::add);
         return links;
     }
 
@@ -111,9 +114,7 @@ public class ScrapingInformationDB {
 
         collectionNames.forEach(collectionName -> {
             database.getCollection(collectionName)
-                    .insertOne(new Document(collections.get(collectionName)), (result, t) -> {
-                        System.out.println("The data got inserted for client - " + clientId + " and job - " + jobId);
-                    });
+                    .insertOne(new Document(collections.get(collectionName)));
         });
     }
 
